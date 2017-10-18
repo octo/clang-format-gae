da8278bf7b1f0eed043084709ee02ca42b5b2af1
Florian Forster <ff@octo.it>
2017-10-18 22:34:33 +0200

    Set a timeout when executing clang-format.
    Also hook STDOUT directly up to the response writer to avoid unnecessary
    copying.

--- clang-format-gae.go
+++ clang-format-gae.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"time"
 )
 
 const (
@@ -22,43 +23,34 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if err := contextHandler(r.Context(), w, r); err != nil {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "https://github.com/octo/clang-format-gae/", http.StatusFound)
+		return
+	}
+
+	if err := format(r.Context(), r.Body, w); err != nil {
 		log.Printf("contextHandler: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-func contextHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	if r.Method != "POST" {
-		http.Redirect(w, r, "https://github.com/octo/clang-format-gae/", http.StatusFound)
-		return nil
-	}
+func format(ctx context.Context, in io.Reader, out io.Writer) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
 
-	formatted, err := format(ctx, r.Body)
-	if err != nil {
-		return err
-	}
-
-	_, err = formatted.WriteTo(w)
-	return err
-}
-
-func format(ctx context.Context, in io.Reader) (*bytes.Buffer, error) {
 	cmd := exec.CommandContext(ctx, clangFormat, "-style=LLVM")
 	cmd.Stdin = in
-
-	out := &bytes.Buffer{}
 	cmd.Stdout = out
 
 	errbuf := &bytes.Buffer{}
 	cmd.Stderr = errbuf
 
 	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("clang-format: %v\nSTDERR: %s", err, errbuf)
+		return fmt.Errorf("clang-format: %v\nSTDERR: %s", err, errbuf)
 	}
 
-	return out, nil
+	return nil
 }
 
 func healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
