892e6b78a0808ce690f2bdca14d4ee2b9e24c139
Florian Forster <ff@octo.it>
2017-10-24 20:49:06 +0200

    Add tracing of clang-format calls.

--- clang-format-gae.go
+++ clang-format-gae.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 	"os/exec"
 	"time"
+
+	"cloud.google.com/go/trace"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 )
 
 const (
@@ -16,8 +20,27 @@ const (
 )
 
 func main() {
+	ctx := context.Background()
+
+	creds, err := google.FindDefaultCredentials(ctx, trace.ScopeTraceAppend)
+	if err != nil {
+		log.Fatalf("FindDefaultCredentials(): %v", err)
+	}
+
+	traceClient, err := trace.NewClient(ctx, creds.ProjectID, option.WithTokenSource(creds.TokenSource))
+	if err != nil {
+		log.Fatalf("trace.NewClient(): %v", err)
+	}
+
+	// sample 100% of requests, but at most 1 per second.
+	policy, err := trace.NewLimitedSampler(1.0, 1.0)
+	if err != nil {
+		log.Fatalf("NewLimitedSampler(): %v", err)
+	}
+	traceClient.SetSamplingPolicy(policy)
+
 	http.HandleFunc("/_ah/health", healthCheckHandler)
-	http.HandleFunc("/", handler)
+	http.Handle("/", traceClient.HTTPHandler(http.HandlerFunc(handler)))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -45,6 +68,9 @@ func format(ctx context.Context, in io.Reader, out io.Writer) error {
 
 	errbuf := &bytes.Buffer{}
 	cmd.Stderr = errbuf
+
+	span := trace.FromContext(ctx).NewChild(clangFormat)
+	defer span.Finish()
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("clang-format: %v\nSTDERR: %s", err, errbuf)
